9f88ebfc2a502e2062e1bc6900313eafcdc03471
AlanAli-byte <alanpali2004@gmail.com>
2026-04-08 14:09:33 +0530

    Update vite.config (1).ts

--- vite.config (1).ts
+++ vite.config (1).ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SoapLife/',   // 👈 ADD THIS LINE
+  base: '/SoapLife/',   // 👈 THIS LINE MUST BE HERE
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
